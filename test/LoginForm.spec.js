b714bd2cd9f74acda0fdfb3aa935231368ede7dd
Shirui Wang <shirui.wang@hotmail.com>
2017-01-28 22:12:04 -0500

    Updated Signup page styling

--- test/LoginForm.spec.js
+++ test/LoginForm.spec.js
@@ -6,39 +6,39 @@ import sinon from 'sinon';
 import LoginForm from '../src/components/LoginForm';
 
 describe('<LoginForm />', () => {
-  it('should render the title as <h2 />', () => {
-    const wrapper = shallow(<LoginForm />);
-    expect(wrapper.find('h2')).to.have.length(1);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<LoginForm />);
+  });
+
+  it('should render the title as <h3 />', () => {
+    expect(wrapper.find('h3')).to.have.length(1);
   });
 
   it('should render the word Login for the title', () => {
-    const wrapper = shallow(<LoginForm />);
-    expect(wrapper.find('h2').text()).to.equal('Login');
+    expect(wrapper.find('h3').text()).to.equal('Login');
   });
 
   it('should render two inputs', () => {
-    const wrapper = shallow(<LoginForm />);
     expect(wrapper.find('input')).to.have.length(2);
   });
 
   it('should render a login button', () => {
-    const wrapper = shallow(<LoginForm />);
     expect(wrapper.find('button')).to.have.length(1);
   });
 
   it('should render a signup link', () => {
-    const wrapper = shallow(<LoginForm />);
-    expect(wrapper.find('h5')).to.have.length(1);
+    expect(wrapper.find('h6')).to.have.length(1);
     expect(wrapper.find('Link')).to.have.length(1);
   });
 
   it('should render the correct text for signup', () => {
-    const wrapper = shallow(<LoginForm />);
-    expect(wrapper.find('h5').text()).to.equal("Don't have an account? Sign up <Link />.");
+    expect(wrapper.find('h6').text()).to.equal("Don't have an account? Sign up <Link />.");
   });
 
   it('should render correct email and password from props passed in', () => {
-    const wrapper = shallow(<LoginForm email={'test'} password={'test'} />);
+    wrapper = shallow(<LoginForm email={'test'} password={'test'} />);
     wrapper.find('input').forEach(function (node) {
       expect(node.props().value).to.equal('test');
     });
@@ -46,7 +46,7 @@ describe('<LoginForm />', () => {
 
   it('calls update on input value after keypress', () => {
     const onChange = sinon.spy();
-    const wrapper = shallow(
+    wrapper = shallow(
       <LoginForm 
         updateEmailInput={onChange} 
         updatePasswordInput={onChange} 
@@ -60,7 +60,7 @@ describe('<LoginForm />', () => {
 
   it('should call function on form submission', () => {
     const onClick = sinon.spy();
-    const wrapper = shallow(<LoginForm handleLogin={onClick} />);
+    wrapper = shallow(<LoginForm handleLogin={onClick} />);
     wrapper.find('form').simulate('submit');
     expect(onClick.calledOnce).to.equal(true);
   });
--- test/SignupForm.spec.js
+++ test/SignupForm.spec.js
@@ -7,34 +7,33 @@ import SignupForm from '../src/components/SignupForm';
 
 describe('<SignupForm />', () => {
   const state = {username: 'test', password: 'test', email: 'test'};
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<SignupForm state={state} />);
+  });
 
   it('should render a title', () => {
-    const wrapper = shallow(<SignupForm state={state} />);
-    expect(wrapper.find('h2')).to.have.length(1);
+    expect(wrapper.find('h4')).to.have.length(1);
   });
 
   it('should render the correct text for the title', () => {
-    const wrapper = shallow(<SignupForm state={state} />);
-    expect(wrapper.find('h2').text()).to.equal('Account Signup');
+    expect(wrapper.find('h4').text()).to.equal('Account Signup');
   });
 
   it('should render 3 inputs', () => {
-    const wrapper = shallow(<SignupForm state={state} />);
     expect(wrapper.find('input')).to.have.length(3);
   });
 
   it('should render 3 input labels', () => {
-    const wrapper = shallow(<SignupForm state={state} />);
     expect(wrapper.find('label')).to.have.length(3);
   });
 
   it('should render a signup button', () => {
-    const wrapper = shallow(<SignupForm state={state} />);
     expect(wrapper.find('button')).to.have.length(1);
   });
 
   it('should render correct values when props passed into input', () => {
-    const wrapper = shallow(<SignupForm state={state} />);
     wrapper.find('input').forEach(function(node) {
       expect(node.props().value).to.equal('test');
     });
@@ -45,7 +44,7 @@ describe('<SignupForm />', () => {
     const updatePass = sinon.spy();
     const updateEmail = sinon.spy();
 
-    const wrapper = shallow(
+    wrapper = shallow(
       <SignupForm 
         state={state}
         updateEmailInput={updateEmail}
@@ -65,7 +64,7 @@ describe('<SignupForm />', () => {
 
   it('should call function on form submission', () => {
     const signup = sinon.spy();
-    const wrapper = shallow(<SignupForm state={state} handleSignup={signup} />);
+    wrapper = shallow(<SignupForm state={state} handleSignup={signup} />);
 
     wrapper.find('form').simulate('submit');
     expect(signup.calledOnce).to.equal(true);
